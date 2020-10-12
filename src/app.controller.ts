e67d8cf1c83056d5a33f5d225de2d409beb88f8f
FrozenCz <info@milanknop.cz>
2020-10-12 19:11:15 +0200

    optional (?) fix for swagger in dto's/entity

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -49,9 +49,4 @@ export class AppController {
 
 
     }
-
-    @Get()
-    getHello(): string {
-        return this.appService.getHello();
-    }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,13 +2,5 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello ..!..!';
-  }
-
-  countUsers(): number {
-
-    return 0;
-  }
 
 }
